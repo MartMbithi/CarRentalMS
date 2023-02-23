3a36be96998e63f3bbde38e4ccfb9af30ce5ff5a
MartMbithi <martdevelopers254@gmail.com>
2023-02-23 16:48:45 +0300

    All Unit Tests Pass On Dashboard Scaffoldings

---
+++ app/partials/back_office_footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="row no-gutters justify-content-between fs--1 mt-4 mb-3">
+        <div class="col-12 col-sm-auto text-center">
+            <p class="mb-0 text-600"> A <a href="https://devlan.co.ke/">Devlan Solutions LTD</a>
+                Production
+            </p>
+        </div>
+        <div class="col-12 col-sm-auto text-center">
+            <p class="mb-0 text-600">v2.0</p>
+        </div>
+    </div>
+</footer>
