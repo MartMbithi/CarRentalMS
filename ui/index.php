e02edbfb019116b854547c87886a1065a1ccc53d
MartMbithi <martdevelopers254@gmail.com>
2023-02-25 11:35:37 +0300

    Remove Main Page Reservations

--- ui/index.php
+++ ui/index.php
@@ -64,6 +64,7 @@
  *   TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.
  *
  */
+include('../app/settings/config.php');
 require_once('../app/partials/landing_head.php');
 ?>
 
@@ -101,37 +102,7 @@ require_once('../app/partials/landing_head.php');
             <div class="row no-gutters">
                 <div class="col-md-12	featured-top">
                     <div class="row no-gutters">
-                        <div class="col-md-4 d-flex align-items-center">
-                            <form method="post" class="request-form ftco-animate bg-primary">
-                                <h2>Make your trip</h2>
-                                <div class="form-group">
-                                    <label for="" class="label">Pick-up location</label>
-                                    <input type="text" class="form-control" placeholder="City, Airport, Station, etc">
-                                </div>
-                                <div class="form-group">
-                                    <label for="" class="label">Drop-off location</label>
-                                    <input type="text" class="form-control" placeholder="City, Airport, Station, etc">
-                                </div>
-                                <div class="d-flex">
-                                    <div class="form-group mr-2">
-                                        <label for="" class="label">Pick-up date</label>
-                                        <input type="text" class="form-control" id="book_pick_date" placeholder="Date">
-                                    </div>
-                                    <div class="form-group ml-2">
-                                        <label for="" class="label">Drop-off date</label>
-                                        <input type="text" class="form-control" id="book_off_date" placeholder="Date">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="" class="label">Pick-up time</label>
-                                    <input type="text" class="form-control" id="time_pick" placeholder="Time">
-                                </div>
-                                <div class="form-group">
-                                    <input type="submit" value="Rent A Car Now" class="btn btn-secondary py-3 px-4">
-                                </div>
-                            </form>
-                        </div>
-                        <div class="col-md-8 d-flex align-items-center">
+                        <div class="col-md-12 d-flex align-items-center">
                             <div class="services-wrap rounded-right w-100">
                                 <h3 class="heading-section mb-4">Better Way to Rent Your Perfect Cars</h3>
                                 <div class="row d-flex mb-4">
