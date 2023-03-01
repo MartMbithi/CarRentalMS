be7defde1cc23d2ac418463ff101ec232b0acd97
MartMbithi <martdevelopers254@gmail.com>
2023-03-01 16:02:09 +0300

    Craft Bulk Import On Back Office Users

--- ui/backoffice_users.php
+++ ui/backoffice_users.php
@@ -236,6 +236,44 @@ require_once('../app/partials/back_office_head.php');
                 </div>
                 <!-- End Add Staff Modal -->
 
+                <!-- Bulk import staffs modal-->
+                <div class="modal fade fixed-right" id="bulkImportStaffs" role="dialog" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered  modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header align-items-center">
+                                <div class="text-center">
+                                    <h6 class="mb-0 text-bold">Bulk import system users</h6>
+                                </div>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form class="needs-validation" method="post" enctype="multipart/form-data" role="form">
+                                    <div class="row">
+                                        <div class="form-group col-md-12">
+                                            <label for="validationTooltip01">
+                                                <a href="../storage/templates/users.xlsx"> Download a template here</a>
+                                            </label>
+                                        </div>
+                                        <div class="form-group col-md-12">
+                                            <label for="validationTooltip01">XLS File</label>
+                                            <div class="custom-file">
+                                                <input type="file" accept=".xlsx" name="staffs_details" required class="custom-file-input" id="inputGroupFile02">
+                                                <label class="custom-file-label" for="customFile">Choose file</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="text-right">
+                                        <button type="submit" name="Bulk_Import_Staffs" class="btn btn-outline-success">Upload staffs</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End bulk import staffs modal -->
+
             </div>
         </div>
     </main><!-- ===============================================-->
