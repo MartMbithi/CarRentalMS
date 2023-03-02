ea49e277528eb28a52a816f855792d0802b6476d
MartMbithi <martdevelopers254@gmail.com>
2023-03-02 12:36:08 +0300

    Craft Users PDF Reports

--- app/reports/pdf/users.php
+++ app/reports/pdf/users.php
@@ -64,3 +64,150 @@
  *   TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.
  *
  */
+
+require_once('../vendor/autoload.php');
+
+use Dompdf\Dompdf;
+
+$dompdf = new Dompdf();
+
+$html =
+    '
+        <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta name="" content="XYZ,0,0,1" />
+                    <style type="text/css">
+                        table {
+                            font-size: 12px;
+                            padding: 4px;
+                        }
+
+                        th {
+                            text-align: left;
+                            padding: 4pt;
+                        }
+
+                        td {
+                            padding: 5pt;
+                        }
+
+                        #b_border {
+                            border-bottom: dashed thin;
+                        }
+
+                        legend {
+                            color: #0b77b7;
+                            font-size: 1.2em;
+                        }
+
+                        #error_msg {
+                            text-align: left;
+                            font-size: 11px;
+                            color: red;
+                        }
+
+                        .header {
+                            margin-bottom: 20px;
+                            width: 100%;
+                            text-align: left;
+                            position: absolute;
+                            top: 0px;
+                        }
+
+                        .footer {
+                            width: 100%;
+                            text-align: center;
+                            position: fixed;
+                            bottom: 5px;
+                        }
+
+                        #no_border_table {
+                            border: none;
+                        }
+
+                        #bold_row {
+                            font-weight: bold;
+                        }
+
+                        #amount {
+                            text-align: right;
+                            font-weight: bold;
+                        }
+
+                        .pagenum:before {
+                            content: counter(page);
+                        }
+
+                        /* Thick red border */
+                        hr.red {
+                            border: 1px solid red;
+                        }
+                        .list_header{
+                            font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+                        }
+                    </style>
+                </head>
+
+                <body style="margin:1px;">
+                    <div class="list_header" align="center">
+                        <h3>
+                        <img src="'.$base64.'" width="290" alt="Logo"> <br> <br>
+                            The Ultimate Car Rental Information Management System
+                        </h3>                        
+                        <hr style="width:100%" , color=black>
+                        <h5>System Users Reports - Generated On '.date('d M Y').'</h5>
+                    </div>
+                    <table border="1" cellspacing="0" width="98%" style="font-size:9pt">
+                        <thead>
+                            <tr>
+                                <th style="width:20%">S/N</th>
+                                <th style="width:50%">Number</th>
+                                <th style="width:100%">Full Names</th>
+                                <th style="width:50%">ID Number</th>
+                                <th style="width:80%">Phone Number</th>
+                                <th style="width:100%">Email Address</th>
+                                <th style="width:50%">Access Level</th>
+                                <th style="width:100%">Physical Address</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            ';
+                            $users_sql = mysqli_query(
+                                $mysqli,
+                                "SELECT * FROM users"
+                            );
+                            $cnt = 1;
+                            if (mysqli_num_rows($users_sql) > 0) {
+                                while ($users = mysqli_fetch_array($users_sql)) {
+                                    $html .=
+                                        '
+                                            <tr>
+                                                <td>' . $cnt . '</td>
+                                                <td>' . $users['user_number'] . '</td>
+                                                <td>' . $users['user_name'] . '</td>
+                                                <td>' . $users['user_id_number'] . '</td>
+                                                <td>' . $users['user_phone_number'] . '</td>
+                                                <td>' . $users['user_email'] . '</td>
+                                                <td>' . $users['user_access_level'] . '</td>
+                                                <td>' . $users['user_address'] . '</td>
+                                            </tr>
+                                        ';
+                                    $cnt = $cnt + 1;
+                                }
+                            }
+                            $html .= '
+                        </tbody>
+                    </table>
+                </body>
+            </html>
+    ';
+$dompdf = new Dompdf();
+$dompdf->load_html($html);
+$dompdf->set_paper('A4', 'landscape');
+$dompdf->set_option('isHtml5ParserEnabled', true);
+$dompdf->render();
+$dompdf->stream('Users Reports Generated On'. date('d M Y'), array("Attachment" => 1));
+$options = $dompdf->getOptions();
+$options->setDefaultFont('');
+$dompdf->setOptions($options);
