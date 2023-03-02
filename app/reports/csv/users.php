af4cf52558b11567a8192c152a878b8d5b9517ef
MartMbithi <martdevelopers254@gmail.com>
2023-03-02 12:11:37 +0300

    All Unit Tests Pass On Users XLS Reports

--- app/reports/csv/users.php
+++ app/reports/csv/users.php
@@ -77,10 +77,10 @@ $report_template = '../storage/templates/reports.xlsx';
 $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
 $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($report_template);
 $sheet = $spreadsheet->getActiveSheet();
-$sheet->setTitle('Systems Users List On' . date('d M Y'), true);
+$sheet->setTitle('Users List On' . date('d M Y'), true);
 
 /* Sheet columns head names */
-$sheet->setCellValue('A5', '#');
+$sheet->setCellValue('A5', 'S/N');
 $sheet->setCellValue('B5', 'User Number');
 $sheet->setCellValue('C5', 'Full Names');
 $sheet->setCellValue('D5', 'National ID Number');
@@ -94,7 +94,7 @@ $sheet->setCellValue('H5', 'Address');
 $query = $mysqli->query("SELECT * FROM users");
 if ($query->num_rows > 0) {
     $cnt = 1;
-    $row = 5;/* Start filling data from row */
+    $row = 6;/* Start filling data from row */
     while ($users = $query->fetch_assoc()) {
         /* Populate cell data */
         $sheet->setCellValue('A' . $row, $cnt);
@@ -110,7 +110,7 @@ if ($query->num_rows > 0) {
     }
 }
 
-$file_name = 'Staffs List On ' . date('d M Y') . '.xlsx';
+$file_name = 'Users List On ' . date('d M Y') . '.xlsx';
 ob_end_clean();
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment;filename=' . $file_name . '');
