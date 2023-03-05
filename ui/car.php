4cc510d3bbea3c74b7e658d22938be3a6d38c55d
MartMbithi <martdevelopers254@gmail.com>
2023-03-05 11:34:06 +0300

    Craft User Images On Car Ratings

--- ui/car.php
+++ ui/car.php
@@ -207,130 +207,53 @@ require_once('../app/partials/landing_head.php');
 
                                     <div class="tab-pane fade" id="pills-review" role="tabpanel" aria-labelledby="pills-review-tab">
                                         <div class="row">
-                                            <div class="col-md-7">
-                                                <h3 class="head">23 Reviews</h3>
-                                                <div class="review d-flex">
-                                                    <div class="user-img" style="background-image: url(images/person_1.jpg)"></div>
-                                                    <div class="desc">
+                                            <div class="col-md-12">
+                                                <h3 class="head text-center">What our clients say about this vehicle</h3>
+                                                <?php
+                                                $ratings_sql = mysqli_query(
+                                                    $mysqli,
+                                                    "SELECT * FROM ratings r
+                                                    INNER JOIN car_rentals cr ON cr.rental_car_id = r.rating_rental_id
+                                                    INNER JOIN clients c ON c.client_id = r.rating_client_id
+                                                    WHERE rental_car_id = '{$_GET['view']}'"
+                                                );
+                                                if (mysqli_num_rows($ratings_sql) > 0) {
+                                                    while ($ratings = mysqli_fetch_array($ratings_sql)) {
+                                                        /* Process user image */
+                                                        if ($ratings['client_dpic'] == '') {
+                                                            $image = "../storage/clients/no-profile.png";
+                                                        } else {
+                                                            $image = "../storage/clients/" . $ratings['client_dpic'];
+                                                        }
+                                                ?>
+                                                        <div class="review d-flex">
+                                                            <div class="user-img" style="background-image: url(<?php echo $image; ?>)"></div>
+                                                            <div class="desc">
+                                                                <h4>
+                                                                    <span class="text-left">Jacob Webb</span>
+                                                                    <span class="text-right">14 March 2018</span>
+                                                                </h4>
+                                                                <p class="star">
+                                                                    <span>
+                                                                        <i class="ion-ios-star"></i>
+                                                                        <i class="ion-ios-star"></i>
+                                                                        <i class="ion-ios-star"></i>
+                                                                        <i class="ion-ios-star"></i>
+                                                                        <i class="ion-ios-star"></i>
+                                                                    </span>
+                                                                </p>
+                                                                <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrov</p>
+                                                            </div>
+                                                        </div>
+                                                    <?php
+                                                    }
+                                                } else { ?>
+                                                    <div class="review d-flex">
                                                         <h4>
-                                                            <span class="text-left">Jacob Webb</span>
-                                                            <span class="text-right">14 March 2018</span>
+                                                            <span class="text-center text-danger">No reviews available for this vehicle.</span>
                                                         </h4>
-                                                        <p class="star">
-                                                            <span>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                            </span>
-                                                            <span class="text-right"><a href="#" class="reply"><i class="icon-reply"></i></a></span>
-                                                        </p>
-                                                        <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrov</p>
                                                     </div>
-                                                </div>
-                                                <div class="review d-flex">
-                                                    <div class="user-img" style="background-image: url(images/person_2.jpg)"></div>
-                                                    <div class="desc">
-                                                        <h4>
-                                                            <span class="text-left">Jacob Webb</span>
-                                                            <span class="text-right">14 March 2018</span>
-                                                        </h4>
-                                                        <p class="star">
-                                                            <span>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                            </span>
-                                                            <span class="text-right"><a href="#" class="reply"><i class="icon-reply"></i></a></span>
-                                                        </p>
-                                                        <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrov</p>
-                                                    </div>
-                                                </div>
-                                                <div class="review d-flex">
-                                                    <div class="user-img" style="background-image: url(images/person_3.jpg)"></div>
-                                                    <div class="desc">
-                                                        <h4>
-                                                            <span class="text-left">Jacob Webb</span>
-                                                            <span class="text-right">14 March 2018</span>
-                                                        </h4>
-                                                        <p class="star">
-                                                            <span>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                            </span>
-                                                            <span class="text-right"><a href="#" class="reply"><i class="icon-reply"></i></a></span>
-                                                        </p>
-                                                        <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrov</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-5">
-                                                <div class="rating-wrap">
-                                                    <h3 class="head">Give a Review</h3>
-                                                    <div class="wrap">
-                                                        <p class="star">
-                                                            <span>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                (98%)
-                                                            </span>
-                                                            <span>20 Reviews</span>
-                                                        </p>
-                                                        <p class="star">
-                                                            <span>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                (85%)
-                                                            </span>
-                                                            <span>10 Reviews</span>
-                                                        </p>
-                                                        <p class="star">
-                                                            <span>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                (70%)
-                                                            </span>
-                                                            <span>5 Reviews</span>
-                                                        </p>
-                                                        <p class="star">
-                                                            <span>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                (10%)
-                                                            </span>
-                                                            <span>0 Reviews</span>
-                                                        </p>
-                                                        <p class="star">
-                                                            <span>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                <i class="ion-ios-star"></i>
-                                                                (0%)
-                                                            </span>
-                                                            <span>0 Reviews</span>
-                                                        </p>
-                                                    </div>
-                                                </div>
+                                                <?php } ?>
                                             </div>
                                         </div>
                                     </div>
