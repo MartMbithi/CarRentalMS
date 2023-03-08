283ea74815a77e998075bfd2263e407849b6a682
MartMbithi <martdevelopers254@gmail.com>
2023-03-08 15:03:44 +0300

    Payments Retails

--- app/reports/pdf/payments.php
+++ app/reports/pdf/payments.php
@@ -65,4 +65,156 @@
  *
  */
 
- 
+
+require_once('../vendor/autoload.php');
+
+use Dompdf\Dompdf;
+
+$dompdf = new Dompdf();
+
+$html =
+    '
+        <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta name="" content="XYZ,0,0,1" />
+                    <style type="text/css">
+                        table {
+                            font-size: 12px;
+                            padding: 4px;
+                        }
+
+                        th {
+                            text-align: left;
+                            padding: 4pt;
+                        }
+
+                        td {
+                            padding: 5pt;
+                        }
+
+                        #b_border {
+                            border-bottom: dashed thin;
+                        }
+
+                        legend {
+                            color: #0b77b7;
+                            font-size: 1.2em;
+                        }
+
+                        #error_msg {
+                            text-align: left;
+                            font-size: 11px;
+                            color: red;
+                        }
+
+                        .header {
+                            margin-bottom: 20px;
+                            width: 100%;
+                            text-align: left;
+                            position: absolute;
+                            top: 0px;
+                        }
+
+                        .footer {
+                            width: 100%;
+                            text-align: center;
+                            position: fixed;
+                            bottom: 5px;
+                        }
+
+                        #no_border_table {
+                            border: none;
+                        }
+
+                        #bold_row {
+                            font-weight: bold;
+                        }
+
+                        #amount {
+                            text-align: right;
+                            font-weight: bold;
+                        }
+
+                        .pagenum:before {
+                            content: counter(page);
+                        }
+
+                        /* Thick red border */
+                        hr.red {
+                            border: 1px solid red;
+                        }
+                        .list_header{
+                            font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+                        }
+                    </style>
+                </head>
+
+                <body style="margin:1px;">
+                    <div class="list_header" align="center">
+                        <h3>
+                        <img src="' . $base64 . '" width="200" alt="Logo"> <br> <br>
+                            The Ultimate Car Rental Information Management System
+                        </h3>                        
+                        <hr style="width:100%" , color=black>
+                        <h5>Rental Payments List - Generated On ' . date('d M Y g:ia') . '</h5>
+                    </div>
+                    <table border="1" cellspacing="0" width="100%" style="font-size:9pt">
+                        <thead>
+                            <tr>
+                                <th style="width:20%">S/N</th>
+                                <th style="width:100%">Rental REF</th>
+                                <th style="width:100%">Payment REF</th>
+                                <th style="width:100%">Vehicle Reg Number</th>
+                                <th style="width:100%">Vehicle Model</th>
+                                <th style="width:100%">Rented On</th>
+                                <th style="width:100%">Amount Paid</th>
+                                <th style="width:100%">Paid</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            ';
+                            $vehicles_sql = mysqli_query(
+                                $mysqli,
+                                "SELECT * FROM payments p
+                                INNER JOIN  car_rentals cr ON cr.rental_id = p.payment_rental_id
+                                INNER JOIN cars c ON c.car_id = cr.rental_car_id
+                                INNER JOIN clients cl ON cl.client_id = cr.rental_client_id"
+                            );
+                            $cnt = 1;
+                            if (mysqli_num_rows($vehicles_sql) > 0) {
+                                while ($vehicles = mysqli_fetch_array($vehicles_sql)) {
+                                    $html .=
+                                        '
+                                            <tr>
+                                                <td>' . $cnt . '</td>
+                                                <td>' . $vehicles['rental_ref_code'] . '</td>
+                                                <td>' . $vehicles['car_reg_number'] . '</td>
+                                                <td>' . $vehicles['car_model'] . '</td>
+                                                <td>' . $vehicles['client_names'] . '</td>
+                                                <td>' . $vehicles['client_phone_number'] . '</td>
+                                                <td>' . date('d M Y', strtotime($vehicles['rental_from_date'])) . '</td>
+                                                <td>' . date('d M Y', strtotime($vehicles['rental_from_date'])) . '</td>
+                                                <td>Ksh' . $vehicles['rental_cost'] . '</td>
+                                                <td>' . $availability . '</td>
+
+                                            </tr>
+                                        ';
+                                    $cnt = $cnt + 1;
+                                }
+                            }
+                            $html .= '
+                        </tbody>
+                    </table>
+                </body>
+            </html>
+    ';
+$dompdf = new Dompdf();
+$dompdf->load_html($html);
+$dompdf->set_paper('A4', 'landscape');
+$dompdf->set_option('isHtml5ParserEnabled', true);
+$dompdf->render();
+$dompdf->stream('Rentals List Generated On' . date('d M Y'), array("Attachment" => 1));
+$options = $dompdf->getOptions();
+$options->setDefaultFont('');
+$dompdf->setOptions($options);
