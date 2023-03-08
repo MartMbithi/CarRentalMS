7f030d3f88c331276a32cbba7dc0577603b9ab14
MartMbithi <martdevelopers254@gmail.com>
2023-03-09 02:06:06 +0300

    Craft Landing Footer Quick Links

--- app/partials/landing_footer.php
+++ app/partials/landing_footer.php
@@ -24,11 +24,10 @@
                 <div class="ftco-footer-widget mb-4 ml-md-5">
                     <h2 class="ftco-heading-2">Information</h2>
                     <ul class="list-unstyled">
-                        <li><a href="#" class="py-2 d-block">About</a></li>
-                        <li><a href="#" class="py-2 d-block">Services</a></li>
-                        <li><a href="#" class="py-2 d-block">Term and Conditions</a></li>
-                        <li><a href="#" class="py-2 d-block">Best Price Guarantee</a></li>
-                        <li><a href="#" class="py-2 d-block">Privacy &amp; Cookies Policy</a></li>
+                        <li><a href="about" class="py-2 d-block">About</a></li>
+                        <li><a href="services" class="py-2 d-block">Services</a></li>
+                        <li><a href="toc" class="py-2 d-block">Term and Conditions</a></li>
+                        <li><a href="privacy" class="py-2 d-block">Privacy &amp; Cookies Policy</a></li>
                     </ul>
                 </div>
             </div>
@@ -36,11 +35,8 @@
                 <div class="ftco-footer-widget mb-4">
                     <h2 class="ftco-heading-2">Customer Support</h2>
                     <ul class="list-unstyled">
-                        <li><a href="#" class="py-2 d-block">FAQ</a></li>
-                        <li><a href="#" class="py-2 d-block">Payment Option</a></li>
-                        <li><a href="#" class="py-2 d-block">Booking Tips</a></li>
-                        <li><a href="#" class="py-2 d-block">How it works</a></li>
-                        <li><a href="#" class="py-2 d-block">Contact Us</a></li>
+                        <li><a href="faq" class="py-2 d-block">FAQ</a></li>
+                        <li><a href="contacts" class="py-2 d-block">Contact Us</a></li>
                     </ul>
                 </div>
             </div>
@@ -50,8 +46,8 @@
                     <div class="block-23 mb-3">
                         <ul>
                             <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                            <li><a href="tel://"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                            <li><a href="mailto://"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
                         </ul>
                     </div>
                 </div>
