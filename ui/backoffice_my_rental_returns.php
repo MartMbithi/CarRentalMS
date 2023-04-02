d47284ebc102a82b5fbaa8a755060ced57c6a1cd
MartMbithi <martdevelopers254@gmail.com>
2023-04-02 18:13:41 +0300

    Implement Back Office Rentals - Show My Rentals

--- ui/backoffice_my_rental_returns.php
+++ ui/backoffice_my_rental_returns.php
@@ -69,7 +69,7 @@ session_start();
 require_once('../app/settings/config.php');
 require_once('../app/settings/client_checklogin.php');
 include('../app/settings/codeGen.php');
-require_once('../app/helpers/rentals.php');
+require_once('../app/helpers/inspection.php');
 require_once('../app/partials/back_office_head.php');
 
 ?>
@@ -92,21 +92,16 @@ require_once('../app/partials/back_office_head.php');
                     </span>
                     <div class="media-body">
                         <h5 class="mb-0 text-primary position-relative">
-                            <span class="bg-200 pr-3">Rentals Module</span>
+                            <span class="bg-200 pr-3">Rentals Return Module</span>
                             <span class="border position-absolute absolute-vertical-center w-100 z-index--1 l-0"></span>
                         </h5>
                         <p class="mb-0 text-justify">
-                            This module allows you to manage your rented vehicles. You can add, edit, delete and view vehicles.
+                            This module allows you to manage your rental vehicles returns. You can add, edit, delete and view returns.
                         </p>
                     </div>
                 </div>
                 <div class="row no-gutters">
                     <div class="card mb-3 col-12">
-                        <div class="card-header text-right">
-                            <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addCategoriesModal">
-                                <i class="fas fa-plus"></i> Add Rentals
-                            </button>
-                        </div>
                         <div class="row">
                             <div class="col-12">
                                 <div class="card-body bg-light">
@@ -114,27 +109,24 @@ require_once('../app/partials/back_office_head.php');
                                         <thead class="bg-200">
                                             <tr>
                                                 <th class="sort">Ref number</th>
-                                                <th class="sort">Vehicle details</th>
-                                                <th class="sort">Client details</th>
-                                                <th class="sort">Rental details</th>
-                                                <th class="sort">Cost</th>
-                                                <th class="sort">Payment status</th>
+                                                <th class="sort">Inspected by</th>
+                                                <th class="sort">Date posted</th>
+                                                <th class="sort">Inspection comments</th>
                                             </tr>
                                         </thead>
                                         <tbody class="bg-white">
                                             <?php
                                             $rentals_sql = mysqli_query(
                                                 $mysqli,
-                                                "SELECT * FROM car_rentals cr 
+                                                "SELECT * FROM rental_returns rr  
+                                                INNER JOIN car_rentals cr ON rr.return_rental_id = cr.rental_id
                                                 INNER JOIN cars c ON c.car_id = cr.rental_car_id
-                                                INNER JOIN clients cl ON cl.client_id = cr.rental_client_id
-                                                WHERE cl.client_id = '{$client_id}'                                    
+                                                INNER JOIN users u ON u.user_id = rr.return_user_id 
+                                                WHERE cr.rental_client_id = '{$client_id}'                                   
                                                 "
                                             );
                                             if (mysqli_num_rows($rentals_sql) > 0) {
                                                 while ($rental = mysqli_fetch_array($rentals_sql)) {
-                                                    $date_now = strtotime(date('Y-m-d'));
-                                                    $return_date = strtotime($rental['rental_to_date']);
                                             ?>
                                                     <tr>
                                                         <td>
@@ -143,28 +135,14 @@ require_once('../app/partials/back_office_head.php');
                                                             </a>
                                                         </td>
                                                         <td>
-                                                            Reg number: <?php echo $rental['car_reg_number']; ?><br>
-                                                            Model: <?php echo $rental['car_model']; ?>
+                                                            Number: <?php echo $rental['user_number']; ?><br>
+                                                            Names: <?php echo $rental['user_name']; ?>
                                                         </td>
                                                         <td>
-                                                            Name: <?php echo $rental['client_names']; ?><br>
-                                                            Contacts: <?php echo $rental['client_phone_number']; ?>
+                                                            <?php echo date('d M Y g:ia', strtotime($rental['return_date_posted'])); ?>
                                                         </td>
                                                         <td>
-                                                            From date: <?php echo date('d M Y', strtotime($rental['rental_from_date'])); ?> <br>
-                                                            To date: <?php echo date('d M Y', strtotime($rental['rental_to_date'])); ?>
-                                                        </td>
-                                                        <td>
-                                                            Kes <?php echo number_format($rental['rental_cost']); ?>
-                                                        </td>
-                                                        <td>
-                                                            <?php
-                                                            if ($rental['rental_payment_status'] == '0') {
-                                                                echo '<span class="badge badge-danger">Pending</span>';
-                                                            } else {
-                                                                echo '<span class="badge badge-success">Paid</span>';
-                                                            }
-                                                            ?>
+                                                            <?php echo $rental['return_comments']; ?>
                                                         </td>
                                                     </tr>
                                             <?php
@@ -177,64 +155,6 @@ require_once('../app/partials/back_office_head.php');
                         </div>
                     </div>
                     <?php require_once('../app/partials/back_office_footer.php'); ?>
-                </div>
-                <!-- Add  Modals -->
-                <div class="modal fade fixed-right" id="addCategoriesModal" role="dialog" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered  modal-xl" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header align-items-center">
-                                <div class="text-center">
-                                    <h6 class="mb-0 text-bold">Register new vehicle rental record</h6>
-                                </div>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <form class="needs-validation" method="post" enctype="multipart/form-data" role="form">
-                                    <div class="row">
-                                        <div class="form-group col-md-6">
-                                            <label for="">Client Details</label>
-                                            <select type="" required name="rental_client_id" class="form-control selectpicker" style="width: 100%;">
-                                                <?php
-                                                $clients_sql = mysqli_query($mysqli, "SELECT * FROM clients WHERE client_id = '{$client_id}'");
-                                                while ($clients = mysqli_fetch_array($clients_sql)) {
-                                                    echo '<option selected value="' . $clients['client_id'] . '">' . $clients['client_names'] . ' - ' . $clients['client_email'] . '</option>';
-                                                }
-                                                ?>
-                                            </select>
-                                        </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Vehicle Details</label>
-                                            <select type="" required name="rental_car_id" class="form-control selectpicker" style="width: 100%;" onchange="GetCarDetails(this.value)" id="CarID">
-                                                <option value="">Select vehicle Registration number</option>
-                                                <?php
-                                                $vehicles_sql = mysqli_query($mysqli, "SELECT * FROM cars WHERE car_availability_status = '0'");
-                                                while ($vehicles = mysqli_fetch_array($vehicles_sql)) {
-                                                    echo '<option value="' . $vehicles['car_id'] . '">' . $vehicles['car_reg_number'] . ' ' . $vehicles['car_model'] . '</option>';
-                                                }
-                                                ?>
-                                            </select>
-                                        </div>
-                                        <!-- Hide this -->
-                                        <input type="hidden" name="rental_cost" id="CarRentalCost">
-                                        <div class="form-group col-md-6">
-                                            <label for="">From date</label>
-                                            <input type="hidden" required name="rental_ref_code" value="<?php echo $ref_code; ?>" class="form-control">
-                                            <input type="date" required name="rental_from_date" class="form-control">
-                                        </div>
-                                        <div class="form-group col-md-6">
-                                            <label for="">Return date</label>
-                                            <input type="date" required name="rental_to_date" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="text-right">
-                                        <button type="submit" name="Add_Rental" class="btn btn-outline-success">Add rental</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
